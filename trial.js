02edf974d57aab33eb5192da030c950e6196fe79
mollyxu <64995721+mollyxu@users.noreply.github.com>
2020-06-06 23:36:16 -0700

    Update trial.js

--- trial.js
+++ trial.js
@@ -1,14 +1,24 @@
-
-
 document.addEventListener('DOMContentLoaded', () => {
-    let elements = []
+    let players = [
+        {name:  "molly", score: 49}, 
+        {name:  "rex", score: 3},
+        {name:  "anunay", score: 1}, 
+        {name:  "ethan", score: 2}];
+    
     let container = document.querySelector('#container')
-    // Add each row to the array
-    container.querySelectorAll('.row').forEach(el => elements.push(el))
+    // let name = document.querySelector(".name")
+    // let score = document.querySelector(".score")
+
     // Clear the container
     container.innerHTML = ''
+
     // Sort the array from highest to lowest
-    elements.sort((a, b) => b.querySelector('.score').textContent - a.querySelector('.score').textContent)
+    players.sort(function(a,b){
+        if (a.score<b.score) return 1;
+        if (a.score>b.score) return -1;
+        return 0;
+    });
     // Put the elements back into the container
-    elements.forEach(e => container.appendChild(e))
+    players.forEach(e => container.append(e.name))
+    players.forEach(e => container.append(e.score))
   })
