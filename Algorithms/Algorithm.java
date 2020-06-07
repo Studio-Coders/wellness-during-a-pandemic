6955aeeda61323db1e7743f5f9829824bb171dcf
rexliu3 <40341857+rexliu3@users.noreply.github.com>
2020-06-06 20:32:44 -0700

    Base Classes created

---
+++ Algorithms/Algorithm.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algorithm {
+    public static void main(String[] args) {
+        // mainList is the main list of Persons
+        List<Person> mainList = new ArrayList<Person>();
+    }
+}
+
---
+++ Algorithms/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    // sleepHours is an array holding the number of hours of sleep each day in one week
+    int[] sleepHours = new int[7];
+
+    // sleepHoursAverage is the average number of hours of sleep in the past week
+    int sleepHoursAverage = 0;
+
+
+    public static void calculateAverage() {
+        int total = 0;
+         
+    }
+
+    public static void 
+
+}
