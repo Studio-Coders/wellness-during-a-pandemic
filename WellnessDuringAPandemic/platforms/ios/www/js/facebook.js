36336bd15829a18bef438a2101519b08b783f5da
rexliu3 <40341857+rexliu3@users.noreply.github.com>
2020-06-06 23:40:20 -0700

    Moved Algorithms

--- WellnessDuringAPandemic/platforms/ios/www/js/facebook.js
+++ WellnessDuringAPandemic/platforms/ios/www/js/facebook.js
@@ -8,3 +8,13 @@ function checkLoginState() {
 FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
 });
+  
+function authResponce() {
+    FB.login(function(response) {
+        if (response.status === 'connected') {
+          // Logged into your webpage and Facebook.
+        } else {
+          // The person is not logged into your webpage or we are unable to tell. 
+        }
+      }, { scope: 'public_profile,email' });
+}
