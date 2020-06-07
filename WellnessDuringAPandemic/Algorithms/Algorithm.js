51f3e134863bfc6af29291f452a777e92070f692
mollyxu <64995721+mollyxu@users.noreply.github.com>
2020-06-07 11:34:33 -0700

    updates

--- WellnessDuringAPandemic/Algorithms/Algorithm.js
+++ WellnessDuringAPandemic/Algorithms/Algorithm.js
@@ -3,19 +3,22 @@ class Algorithm {
         // mainList is the main list of Persons
         const mainList = [new Person("Tom", [5, 6, 7, 8, 3, 4, 3], [23, 22.5, 21, 20, 21.75, 23.5, 24])];
 
-        //const mainNames = [];
-        //const mainScores = [];
+        const mainNames = [];
+        const mainScores = [];
 
-        // Array of objects
-        const players = [];
+        var players = [];
 
         for (let i = 0; i < mainList.length; i++) {
-            players.name.push(mainList[i].name);
-            players.score.push(mainList[i].sleepScore);
+            players.push({name: mainList[i].name, score:mainList[i].sleepScore});
         }
+        // angular.forEach(scope.Algorithm, function (value, key) {
+        //         mainScores.push(value);
+        //         mainNames.push(key);
+        //         players.push({name :key, score:value});
+//  })
+
 
     }
-}
 
-main();
-console.log("hello");
+    main()
+}
