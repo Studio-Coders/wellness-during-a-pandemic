e44fceb259dd4ffdb96f3a01ea19b52979794974
rexliu3 <40341857+rexliu3@users.noreply.github.com>
2020-06-06 22:46:38 -0700

    Fixed a problem

---
+++ Algorithms/Algorithm.js
@@ -0,0 +1,17 @@
+class Algorithm {
+    main() {
+        // mainList is the main list of Persons
+        const mainList = [new Person("Tom", [5, 6, 7, 8, 3, 4, 3], [23, 22.5, 21, 20, 21.75, 23.5, 24])];
+        
+        const mainNames = [];
+        const mainScores = [];
+        
+        for (let i = 0; i < mainList.length; i++) {
+            mainNames.push(mainList[i].name);
+            mainScores.push(mainList[i].sleepScore);
+        }
+
+    }
+
+    main()
+}
