ab2a823f5a4cbb3c1c5b30bb024b13827dad23fc
rexliu3 <40341857+rexliu3@users.noreply.github.com>
2020-06-06 22:55:30 -0700

    Format Files

--- Algorithms/Person.js
+++ Algorithms/Person.js
@@ -4,25 +4,25 @@ export class Person {
 
         // sleepHours is an array holding the number of hours of sleep each day in one week
         let sleepHours = this.calculateSleepHours(sleepStart, sleepEnd);
-    
+
         // sleepHoursAverage is the average number of hours of sleep in the past week
         let sleepHoursAverage = this.calculateAverage(sleepHours);
 
         let sleepScore = this.calculateSleepScore(sleepHours, sleepHoursAverage, sleepStart, sleepEnd);
     }
-   
+
     // Returns the average hours of sleep with int[] as input
     calculateAverage(sleepHoursInput) {
         let total = 0;
         let listLength = sleepHoursInput.length;
-        
+
         for (let i = 0; i < listLength; i++) {
             if (sleepHour != 0) {
                 total += sleepHour;
             }
         }
-            
-        return Math.floor(total/listLength);
+
+        return Math.floor(total / listLength);
     }
 
     // Returns the number of hours of sleep as an int[]. Takes 2 int[] as input.
@@ -49,7 +49,7 @@ export class Person {
         let consistencyUp = this.standardDeviation(sleepEnd);
         consistencyDown = 1 / consistencyDown;
         consistencyUp = 1 / consistencyUp;
-        
+
         let averageSleepScore = 0;
         if (sleepHoursAverage >= 8) {
             averageSleepScore = sleepHoursAverage + ((sleepHoursAverage - 8) * 1.25);
@@ -59,33 +59,33 @@ export class Person {
 
         let consistencyHours = this.standardDeviation(sleepHours);
         consistencyHours = 1 / consistencyHours;
-        
+
         return averageSleepScore * consistencyHours * consistencyUp * consistencyDown * 100;
     }
 
-    standardDeviation(values){
+    standardDeviation(values) {
         var avg = average(values);
-        
-        var squareDiffs = values.map(function(value){
-          var diff = value - avg;
-          var sqrDiff = diff * diff;
-          return sqrDiff;
+
+        var squareDiffs = values.map(function (value) {
+            var diff = value - avg;
+            var sqrDiff = diff * diff;
+            return sqrDiff;
         });
-        
+
         var avgSquareDiff = average(squareDiffs);
-      
+
         var stdDev = Math.sqrt(avgSquareDiff);
         return stdDev;
-      }
-      
-        average(data){
-        var sum = data.reduce(function(sum, value){
-          return sum + value;
+    }
+
+    average(data) {
+        var sum = data.reduce(function (sum, value) {
+            return sum + value;
         }, 0);
-      
+
         var avg = sum / data.length;
         return avg;
-      }
+    }
 
 
 }
